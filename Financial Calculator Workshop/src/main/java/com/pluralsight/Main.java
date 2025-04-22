f71e92b5f93b13ff54325602161ffde0605b2c3f
thejaylajones <janeissaj@gmail.com>
2025-04-22 14:36:52 -0400

    checkin fc

---
+++ Financial Calculator Workshop/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+//        Calculator 1: A mortgage calculator - it is used to calculate out
+//        how much a monthly payment for a loan would be (minus any
+//        insurance or taxes), as well as how much interest you would pay
+//        over the life of the loan.
+//        a. It would accept the principal, interest rate, and loan length//       from the user
+//        b. It would display the expected monthly payment and total
+//        interest paid
+
+
+
+
+
+
+
+    }
+}
